160b32449056f63c1e5ac8e48f4c6c5af789f647
Toey <teayvvv5@gmail.com>
2016-02-06 15:05:46 +0700

    Finish 2/6/16

--- app/src/main/java/com/example/teay/myshop/MainActivity.java
+++ app/src/main/java/com/example/teay/myshop/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.teay.myshop;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,8 +27,45 @@ public class MainActivity extends AppCompatActivity {
         //Clean data
         cleanData();
 
+        //Synchronize JSON to SQLite
+        synJSONtoSQLite();
+
 
     }//main method
+
+    private void synJSONtoSQLite() {
+
+        //change Policy
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myPolicy);
+
+        int intTable = 1;
+        String tag = "Restaurant";
+
+        while (intTable <= 2) {
+
+            //1. Create InputStream(การโหลดไป ประมวลผลไป)
+            InputStream objInputStream = null;
+            String strURLuser = "http://swiftcodingthai.com/6feb/php_get_data_toey.php";
+            String strURLfood = "http://swiftcodingthai.com/6feb/php_get_data_food.php";
+
+            try {
+
+            } catch (Exception e) {
+                Log.d(tag, "InputStream ==> " + e.toString());
+            }
+
+            //2. Create JSON String(เอาสิ่งที่โหลดเปลี่ยนเป็นสตริง)
+
+            //3. Update SQLite(เปลี่ยนข้อมูลให้อัพโหลดได้ และอัพขึ้น SQLite๗
+
+            intTable += 1;
+
+
+        }//while
+
+    }//synJSONtoSQLite
 
     private void cleanData() {
 

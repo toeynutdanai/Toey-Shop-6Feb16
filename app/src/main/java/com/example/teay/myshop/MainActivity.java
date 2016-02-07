08a00cf3a864b8e055b6c49bcc3455bf459a6547
Toey <teayvvv5@gmail.com>
2016-02-07 10:08:13 +0700

    Syn json Finish

--- app/src/main/java/com/example/teay/myshop/MainActivity.java
+++ app/src/main/java/com/example/teay/myshop/MainActivity.java
@@ -6,7 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,16 +59,91 @@ public class MainActivity extends AppCompatActivity {
             InputStream objInputStream = null;
             String strURLuser = "http://swiftcodingthai.com/6feb/php_get_data_toey.php";
             String strURLfood = "http://swiftcodingthai.com/6feb/php_get_data_food.php";
+            HttpPost objHttpPost = null;
 
             try {
+
+                HttpClient objHttpClient = new DefaultHttpClient();
+                switch (intTable) {
+
+                    case 1:
+                        objHttpPost = new HttpPost(strURLuser);
+                        break;
+                    case 2:
+                        objHttpPost = new HttpPost(strURLfood);
+                        break;
+                    default:
+                        break;
+                } //switch
+
+                HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+                HttpEntity objHttpEntity = objHttpResponse.getEntity();
+                objInputStream = objHttpEntity.getContent();
 
             } catch (Exception e) {
                 Log.d(tag, "InputStream ==> " + e.toString());
             }
 
             //2. Create JSON String(เอาสิ่งที่โหลดเปลี่ยนเป็นสตริง)
+            String strJSON = null;
+
+            try {
+
+                BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8")); //ขนย้ายเชือก
+                StringBuilder objStringBuilder = new StringBuilder(); //ผูกเชือก
+                String strLine = null;
+
+                while ((strLine = objBufferedReader.readLine()) != null ) {
+                    objStringBuilder.append(strLine);
+
+
+                } //while
+
+                objInputStream.close();
+                strJSON = objStringBuilder.toString();
+
+
+
+            } catch (Exception e) {
+                Log.d(tag, "strJSON ==> " + e.toString());
+            }
 
             //3. Update SQLite(เปลี่ยนข้อมูลให้อัพโหลดได้ และอัพขึ้น SQLite๗
+            try {
+
+                JSONArray objJsonArray = new JSONArray(strJSON);
+                for (int i = 0; i < objJsonArray.length(); i++) {
+
+                    JSONObject jsonObject = objJsonArray.getJSONObject(i);
+
+                    switch (intTable) {
+                        case 1:
+
+                            //Get Value From userTABLE
+                            String strUser = jsonObject.getString(MyManage.column_user);
+                            String strPassword = jsonObject.getString(MyManage.column_pass);
+                            String strName = jsonObject.getString(MyManage.column_name);
+
+                            objMyManage.addNewValue(0, strUser, strPassword, strName);
+
+                            break;
+                        case 2:
+
+                            //Get Value From foodTABLE
+                            String strFood = jsonObject.getString(MyManage.column_food);
+                            String strPrice = jsonObject.getString(MyManage.column_price);
+                            String strSource = jsonObject.getString(MyManage.column_sorce);
+
+                            objMyManage.addNewValue(1, strFood, strPrice, strSource);
+
+                            break;
+                    }
+
+                } //for
+
+            } catch (Exception e) {
+                Log.d(tag, "Update ==>" + e.toString());
+            }
 
             intTable += 1;
 
